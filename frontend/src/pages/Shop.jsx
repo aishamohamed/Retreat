968ddb5a50e2c625ea51d0664ca0a2b9d5195307
YvonneAonyango <166454264+YvonneAonyango@users.noreply.github.com>
2024-05-04 19:26:42 +0200

    Update Shop.jsx
    page header updated

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Shop() {
   return (
     <div>
-      <h2>Welcome to Our Store</h2>
+      <h2>Welcome Celle Retreat Store</h2>
       <p>At Couple Retreats online shop, we are dedicated to providing high-quality products that enhance your relationship and promote intimacy. From romantic gifts to couple activities, we have everything you need to strengthen your bond and create lasting memories together.</p>
       
       {/* Add your shop content here */}
