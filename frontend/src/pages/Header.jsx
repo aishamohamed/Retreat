6ecb4d660c42412d2bf7c6dbf5f2bc7eea1bfbf4
Aisha <Aishamohamed778899@gmail.com>
2024-05-06 20:24:02 +0200

    added navbar
    styled the navbar and body to match the color canva

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -1,22 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../logo.ico';
 
 function Header() {
   return (
     <header>
-      <h1>Celle Retreats</h1>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/retreat">Retreat</Link></li>
-          <li><Link to="/shop">Shop</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/register">Sign Up</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
-      </nav>
+      <div className="navbar">
+        <img src={logo} alt="Celle Retreats Logo" className="navbar-logo"/>
+        <h1 className="logo">CELLE RETREATS</h1>
+        <nav>
+          <ul>
+            <li><Link to="/" className="nav-link">HOME</Link></li>
+            <li><Link to="/about" className="nav-link">ABOUT</Link></li>
+            <li><Link to="/retreat" className="nav-link">RETREATES</Link></li>
+            <li><Link to="/shop" className="nav-link">SHOP</Link></li>
+            <li><Link to="/contact" className="nav-link">CONTACT</Link></li>
+            <li><Link to="/register" className="nav-link">SIGN UP</Link></li>
+            <li><Link to="/login" className="nav-link">LOGIN</Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
