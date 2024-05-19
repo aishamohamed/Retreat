2c28d8265f4354110d9cfc1b9cf4a6d31770d1e5
Aisha <Aishamohamed778899@gmail.com>
2024-05-19 11:54:07 +0200

    Styled dashboard and added signout btn

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import '../style/userDashboard.css';
 const Dashboard = () => {
   const [userData, setUserData] = useState(null);
   const [upcomingBookings, setUpcomingBookings] = useState([]);
@@ -19,7 +19,7 @@ const Dashboard = () => {
 
         // Fetch bookings 
         const upcomingResponse = await axios.get('http://localhost:3500/bookings/upcoming', {
-          headers: { 'Authorization': `Bearer ${token}` }
+            headers: { 'Authorization': `Bearer ${token}` }
         });
         setUpcomingBookings(upcomingResponse.data);
       } catch (error) {
@@ -35,32 +35,35 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
-      <h2>Dashboard</h2>
-      <p>Welcome, {userData.username}!</p>
+    <div className='d-container'>
+        <div className='dashboard-container'>
+            <h2>Dashboard</h2>
+            <p>Welcome, {userData.username}!</p>
 
-      <div>
-        <h3>Your Profile</h3>
-        <p>Email: {userData.email}</p>
-        {/* Add more user profile information here */}
-      </div>
+            <div className='user-information'>
+                <h3>Your Profile</h3>
+                <p>Email: {userData.email}</p>
+                <p>Username: {userData.username}</p>
+                {/* Add more user profile information here */}
+            </div>
 
-      <div>
-        <h3>Upcoming Bookings</h3>
-        {upcomingBookings.length > 0 ? (
-          <ul>
-            {upcomingBookings.map((booking, index) => (
-              <li key={index}>
-                <p>Date: {new Date(booking.date).toLocaleDateString()}</p>
-                <p>Location: {booking.location}</p>
-                <p>Description: {booking.description}</p>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No upcoming bookings.</p>
-        )}
-      </div>
+            <div className='upcoming-bookings'>
+                <h3>Upcoming Bookings</h3>
+                {upcomingBookings.length > 0 ? (
+                    <ul>
+                    {upcomingBookings.map((booking, index) => (
+                        <li key={index}>
+                        <p>Date: {new Date(booking.date).toLocaleDateString()}</p>
+                        <p>Location: {booking.location}</p>
+                        <p>Description: {booking.description} </p>
+                        </li>
+                    ))}
+                    </ul>
+                ) : (
+                    <p className='no-bookings'>No upcoming bookings.</p>
+                )}
+            </div>
+        </div>
     </div>
   );
 }
