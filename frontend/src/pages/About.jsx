9a76ca654a8d62a709f46c8b5b2340c5a515eb2f
YvonneAonyango <166454264+YvonneAonyango@users.noreply.github.com>
2024-05-20 20:39:15 +0200

    Updated frontend files and vite config

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,10 +10,10 @@ function About() {
         <h2 className='about-header'>About Us</h2>
         <p className='p1'>Welcome to Celle Retreat, where we believe in the power of love and the importance of nurturing relationships. Meet the members behind Celle Retreat!</p>
          
-         <p>Aisha.</p>
-         <p>Rauda.</p>
-         <p>Eszter.</p>
-         <p>Yvonne Onyango - responsible for database and backend part of the project.</p>
+         <p>Aisha - responsible for backend  authentications, dashboard route,  frontend registration, payment functionality and part of css styling.</p>
+         <p>Rauda - responsible for frontend design and styling of  pages such home, header, footer, shop, review pages with css and canva.</p>
+         <p>Eszter - responsible for frontend pages such as the login, cart's frontend authentication and functionalities, also part of css styling</p>
+         <p>Yvonne  - responsible for the database , connecting Mongodb to the server, implementing routes and models.</p>
         <p>We cannot wait to welcome you !</p>
       </div>
     </div>
