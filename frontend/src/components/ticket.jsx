cbaf03123592922f1f1d1262bdcd46d77c671279
esz366 <eszter.kalmar0005@stud.hkr.se>
2024-05-20 23:50:00 +0200

    Added date picker to cart item

--- frontend/src/components/ticket.jsx
+++ frontend/src/components/ticket.jsx
@@ -3,8 +3,9 @@ import { useState } from 'react';
 import "../style/ticket.css";
 
 export default function Ticket(props) {
-    const { id, type, price, currency, daysValid, inCart, location, removeFromCart } = props;
+    const { id, type, price, currency, daysValid, inCart, location, removeFromCart, onDateChange } = props;
     const [addedToCart, setAddedToCart] = useState(false);
+    const [selectedDate, setSelectedDate] = useState('');
 
     const addToCart = async (ticketKey) => {
         try {
@@ -18,35 +19,46 @@ export default function Ticket(props) {
         }
     };
 
+    const handleDateChange = (e) => {
+        setSelectedDate(e.target.value);
+        onDateChange(id, e.target.value);
+    };
+
     return (
-        <div className="ticket" onClick={() => !removeFromCart ? addToCart(id) : removeFromCart(id)}>
+        <div className="ticket">
             <img src={`/${type.toLowerCase()}.jpg`} className="t-image" alt={`${type} icon`} />
             <div className="disc">
                 <h3 className="title-item">{type.toUpperCase()}</h3>
 
-                <div className="item-con">
-                    <div className="ticket-item">
-                        <img src="/calendar.svg" alt="calendar" />
-                        <div className="type-item">{daysValid} days duration</div>
+                <div className="disc-body">
+                    <div className="item-con">
+                        <div className="ticket-item">
+                            <img src="/calendar.svg" alt="calendar" />
+                            <div className="type-item">{daysValid} days duration</div>
+                        </div>
+                        <div className="ticket-item">
+                            <img src="/location.svg" alt="location" />
+                            <div className="type-item">{location}</div>
+                        </div>
+                        <div className="ticket-price">
+                            <img src="/price-tag.svg" alt="price-tag" />
+                            <div className="type-item">{price} {currency}</div>
+                        </div>
                     </div>
-                    <div className="ticket-item">
-                        <img src="/location.svg" alt="location" />
-                        <div className="type-item">{location}</div>
-                    </div>
-                    <div className="ticket-price">
-                        <img src="/price-tag.svg" alt="price-tag" />
-                        <div className="type-item">{price} {currency}</div>
-                    </div>
+
+                    {onDateChange && (<div className="date-picker">
+                        <div className="date-picker">
+                            <input
+                                type="date"
+                                id="date-picker-input"
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                            />
+                        </div>
+                    </div>)}
                 </div>
             </div>
-           {/* <div className="disc">
-                <h3 className="title-item">{type.toUpperCase()}</h3>
-                <div className="item-con">
-                    <div className="type-item">{daysValid} days duration</div>
-                    <div className="type-item">{price} {currency}</div>
-                </div>
-    </div>*/}
-
+           
             { !removeFromCart ? (
                 <button className="buy-btn" onClick={() => addToCart(id)} disabled={inCart || addedToCart}>
                     {inCart || addedToCart ? "Added to cart" : "Add to cart"}
